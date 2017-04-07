5c96d3918a18ce1fe872eb25a43fc3f2654096f5
DavidMealha <davidmealha@hotmail.com>
2017-04-07 02:36:34 +0100

    forgotten changes

--- TP1/MCHAT/src/FileHandler.java
+++ TP1/MCHAT/src/FileHandler.java
@@ -1,14 +1,48 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.NoSuchPaddingException;
 
 //classe para fazer o handling dos ficheiros de configuração
 public class FileHandler {
+	
+	public static void writeCiphersuiteFile(String filename) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException{
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+			
+			bw.write(Utils.toHex(CipherHandler.cipherText("ABCABCABC".getBytes())));
+			
+			// no need to close it.
+			//bw.close();
 
-	public static void readConfigurationFile(String filename){
-		
+			System.out.println("Done");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
 	}
 	
+	public static void readCiphersuiteFile(String filename){
+		//read the ciphersuite file
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line = br.readLine(); 
+			while (line != null) {
+				
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//throw error when the operation mode is ECB, because it's not secure
 	public static PBEConfiguration readPBEncryptionFile(String filename){
 		PBEConfiguration pbe = new PBEConfiguration();
 		
--- TP1/MCHAT/src/KeyGenerator.java
+++ TP1/MCHAT/src/KeyGenerator.java
@@ -1,7 +1,12 @@
 
 public class KeyGenerator {
 
-	public byte[] generateSalt(){
+	public static byte[] generateSalt(){
 		return new byte[10];
+	}
+	
+	public void generateCiphersuite(String password){
+		//the password needs to firstly be hashed, in order to later be compared with the hash of the pwd in the server
+		
 	}
 }
